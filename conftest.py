e0da2190a7ff4925c623862efc64d038bf3d5331
Max <91092934+JetiMax@users.noreply.github.com>
2022-04-07 00:46:08 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,5 +1,10 @@
+import warnings
+import unicodedata
 import pytest
 import uuid
+from selenium import webdriver
+driver = webdriver.Chrome()
+pytestmark = pytest.mark.filterwarnings("error")
 
 
 @pytest.hookimpl(hookwrapper=True, tryfirst=True)
@@ -36,9 +41,9 @@ def web_browser(request, selenium):
             print('URL: ', browser.current_url)
             print('Browser logs:')
             for log in browser.get_log('browser'):
-                print(log)
+                print(log, "utf8")
         except:
-            pass # just ignore any errors here
+            pass  # just ignore any errors here
 
 
 def get_test_case_docstring(item):
@@ -95,72 +100,38 @@ def pytest_collection_finish(session):
 
 
 @pytest.fixture(autouse=True)
+def testind():
+    pytest.driver = webdriver.Chrome('chrome/chromedriver.exe')
+    pytest.driver.get('http://petfriends1.herokuapp.com/login')
+    yield
+    pytest.driver.quit()
+
+
 def test_my_pets():
     pytest.driver.find_element_by_id('email').send_keys('jetimax@yandex.ru')
     pytest.driver.find_element_by_id('pass').send_keys('1725maksim')
-    pytest.driver.find_element_by_css_selector("//button[@type='submit']").click()
+    pytest.driver.find_element_by_css_selector('button[type="submit"]').click()
     assert pytest.driver.find_element_by_tag_name('h1').text == "PetFriends"
-    pytest.driver.find_element_by_css_selector("div#navbarNav > ul > li > a").click()
 
-    images = pytest.driver.find_element_by_css_selector('div#all_my_pets > table > tbody > tr > th > img')
-    # name = pytest.driver.find_element_by_css_selector('div#all_my_pets > table > tr > td')
-    name = pytest.driver.find_element_by_xpath('//input[@id="фыв"]')
-    description = pytest.driver.find_element_by_css_selector('')
+    images = pytest.driver.find_element_by_css_selector('.card-deck.card-img-top')
+    names = pytest.driver.find_element_by_css_selector('.card-deck.card-title>baks</h5>')
+    description = pytest.driver.find_element_by_css_selector('.card-deck.card-text')
 
-    for i in range(len(name)):
-        assert images[i].get_atribute('src') !=''
-        assert name[i].text !=''
-        assert description[i].text !=''
+    for i in range(len(names)):
+        assert images[i].get_atribute('src') != ''
+        assert names[i].text != ''
+        assert description[i].text != ''
         assert ', ' in description[i]
         parts = description[i].text.split(", ")
         assert len(parts[0]) > 0
         assert len(parts[1]) > 0
 
-#Для того, чтобы в нашей фикстуре мы узнали, как именно завершился тест (удачно или нет), мы используем способ, 
-# который в официальных рекомендациях к pytest называется "Making test result information available in fixtures".
-# Для этого мы добавляем вначале функцию pytest_runtest_makereport, которая добавляет к объекту request информацию о результатах теста.
-# Сейчас мы не будем подробно останавливаться на содержании этой функции, но оно стандартно и может быть использовано «как есть».
-# Главное, что мы должны вынести для себя — это возможность использовать проверку переменной request.node.rep_call.failed.
-# Не забывайте добавлять объект request ко входящим параметрам вашей фикстуры
 
-import pytest
-import uuid
+def api_v1():
+    warnings.warn(DeprecationWarning("DeprecationWarning"))
+    return 1
 
 
-@pytest.hookimpl(hookwrapper=True, tryfirst=True)
-def pytest_runtest_makereport(item, call):
-    # This function helps to detect that some test failed
-    # and pass this information to teardown:
-
-    outcome = yield
-    rep = outcome.get_result()
-    setattr(item, "rep_" + rep.when, rep)
-    return rep
-
-@pytest.fixture
-def web_browser(request, selenium):
-
-    browser = selenium
-    browser.set_window_size(1400, 1000)
-
-    # Return browser instance to test case:
-    yield browser
-
-    # Do teardown (this code will be executed after each test):
-
-    if request.node.rep_call.failed:
-        # Make the screen-shot if test failed:
-        try:
-            browser.execute_script("document.body.bgColor = 'white';")
-
-            # Make screen-shot for local debug:
-            browser.save_screenshot('screenshots/' + str(uuid.uuid4()) + '.png')
-
-            # For happy debugging:
-            print('URL: ', browser.current_url)
-            print('Browser logs:')
-            for log in browser.get_log('browser'):
-                print(log)
-
-        except:
-            pass # just ignore any errors here
+@pytest.mark.filterwarnings("DeprecationWarning")
+def test_one():
+    assert api_v1() == 1
